1f6bc84ddc4f08b85f72d541a44f23f3ec15e9ec
davidmolina2810 <davidmolina2810@gmail.com>
2020-11-25 23:43:19 -0600

    commit tree 4a1e60db955530997f8d2c8d953753763e48baea

--- index.js
+++ index.js
@@ -1,1 +1,65 @@
-// Your code here
+const mapToNegativize = (arr) => {
+  let negArr = []
+  arr.forEach(el => {
+    let x = el * -1
+    negArr.push(x)
+  })
+  return negArr
+}
+
+const mapToNoChange = (arr) => {
+  let newArr = []
+  arr.forEach(el => {
+    newArr.push(el)
+  })
+  return newArr
+}
+
+const mapToDouble = (arr) => {
+  let doubled = []
+  arr.forEach(el => {
+    let x = el * 2
+    doubled.push(x)
+  })
+  return doubled
+}
+
+const mapToSquare = (arr) => {
+  let squared = []
+  arr.forEach(el => {
+    let x = el ** 2
+    squared.push(x)
+  })
+  return squared
+}
+
+const reduceToTotal = (arr, start=0) => {
+  let total = start
+  arr.forEach(el => {
+    total += el
+  })
+  return total
+}
+
+const reduceToAllTrue = (arr) => {
+  let count = 0 
+  arr.forEach(el => {
+    if (!!el === true) {
+      count += 1
+    }
+  })
+  if (count === arr.length) {
+    return true
+  }
+  return false
+}
+
+const reduceToAnyTrue = (arr) => {
+  let bool = false
+  arr.forEach(el => {
+    if (!!el === true) {
+      bool = true
+    }
+  })
+  return bool
+}
